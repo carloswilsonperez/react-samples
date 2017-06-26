7e12911889d7496fe5d55b0fdd22116fcea7acf1
Carlos Alberto Wilson Perez <carlos wilson@tiempodev.local>
2017-06-26 13:31:26 -0500

    Moving routes to separate file

--- example01/src/Routes.js
+++ example01/src/Routes.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import {Route, Router} from 'react-router';
+import { Router, Route, hashHistory } from 'react-router'
 import App from './components/App';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import ContactParams from './components/contact/ContactParams';
 
-//let DefaultRoute = Router.DefaultRoute;
 class Routes extends Component {
   render() {
     return (
-      <div>
-        <App>
-          <Router>
-            <Route path="/" component={Home} />
-            <Route path="/about" component={About} />
-          </Router>
-        </App>
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          {/* make them children of `App` */}
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:userName/:id" component={ContactParams} />
+        </Route>
+      </Router>
     );
   }
 }
--- example01/src/index.js
+++ example01/src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Import routing components
-import { Router, Route, hashHistory } from 'react-router'
 import Routes from './Routes';
-import App from './components/App';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import ContactParams from './components/contact/ContactParams';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      {/* make them children of `App` */}
-      <Route path="/home" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/:userName/:id" component={ContactParams} />
-    </Route>
-  </Router>
-  , document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
