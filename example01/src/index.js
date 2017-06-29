70437c925c93e666aa829ec80df3a936b97f938f
Carlos Alberto Wilson Perez <carlos wilson@tiempodev.local>
2017-06-29 11:06:08 -0500

    Adding babel-polyfill to index.js

--- example01/src/index.js
+++ example01/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { render } from 'react-dom';
+import 'babel-polyfill';
 // Import routing components
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
