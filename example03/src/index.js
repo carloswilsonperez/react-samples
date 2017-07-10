524597206bd63ff85a4d70cf496e23e3d32c50be
Carlos Wilson <carlos.wilson@unosquare.com>
2017-07-10 13:58:20 -0500

    Removing whitespace

--- example03/src/index.js
+++ example03/src/index.js
@@ -1,14 +1,11 @@
 import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-//import { render } from 'react-dom';
 import 'babel-polyfill';
 // Import routing components
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
 import { observable } from 'mobx';
-import { observer } from 'mobx-react';
 
 class Counter extends Component {
 
