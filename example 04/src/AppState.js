d9dccde01accc26d5cd9867c0d784b129c8993b4
Carlos Wilson <carlos.wilson@unosquare.com>
2017-07-10 14:31:29 -0500

    Deleting node modules

---
+++ example 04/src/AppState.js
@@ -0,0 +1,19 @@
+import { observable, computed, action } from 'mobx';
+
+class AppState {
+  @observable count = 0;
+
+  @action increment() {
+    this.count++;
+  }
+
+  @action decrement () {
+    this.count--;
+  }
+
+  @computed get duplicateValue() {
+    return this.count * 2;
+  }
+}
+
+export default AppState;
---
+++ example 04/src/index.jsx
@@ -0,0 +1,35 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import AppState from './AppState';
+import App from './App';
+
+//Separation of Concerns for the App:
+
+// 1.- Observable States
+const appState = new AppState();
+
+
+// 2.- Components that react to it: App.jsx
+
+ReactDOM.render(
+    <AppContainer>
+        <App store={appState} />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+
+        render(
+            <AppContainer>
+                <NextApp appState={appState} />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
