1a9fbe6deff93cea7bcd80dcc79ce9163c3d08ee
Carlos Wilson <carlos.wilson@unosquare.com>
2017-07-10 13:34:29 -0500

    Adding mobx react edited boilerplate

---
+++ example03/src/index.js
@@ -0,0 +1,38 @@
+import React, { Component} from 'react';
+import ReactDOM from 'react-dom';
+//import { render } from 'react-dom';
+import 'babel-polyfill';
+// Import routing components
+import Routes from './Routes';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Counter extends Component {
+
+    @observable count = 0;
+
+    render () {
+        return (
+            <div>
+                Counter: {this.count} <br />
+                <button onClick={this.handleInc}>+</button>
+                <button onClick={this.handleDec}>-</button>
+            </div>
+        );
+
+    }
+
+    handleInc = () => {
+        this.count++;
+    };
+
+    handleDec = () => {
+        this.count--;
+    };
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+registerServiceWorker();
